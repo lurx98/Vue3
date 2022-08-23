63267e8e5974e95a772bc1e105c1fac90062c43a
lurx98 <lurx98@gmail.com>
2022-08-23 09:33:07 +0800

    teleport内置组件

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,26 @@
+<template>
+  <div class="app">
+    <h3>我是标题</h3>
+    <h1>我是一级标题</h1>
+    <div id="test">
+      <!-- to属性的值为选择器，表示放在哪个节点下面 -->
+      <teleport to="body">
+        <Dialog></Dialog>
+      </teleport>
+    </div>
+  </div>
+</template>
 <script>
-import { reactive, watch } from 'vue'
-
+import Dialog from './components/Dialog.vue'
 export default {
-  name: 'App',
-  setup(props, context) {
-    const person = reactive({
-      name: 'lu',
-      age: 19,
-    })
-    watch(person, (newV, oV) => {
-      console.log(newV, oV)
-    })
-    return {
-      person,
-    }
+  components: {
+    Dialog,
   },
 }
 </script>
-
-<template>
-  <div @click="person.age++">{{ person.name }}======{{ person.age }}</div>
-</template>
-
-<style scoped></style>
+<style scoped>
+.app {
+  background-color: #eee;
+  padding: 30px;
+}
+</style>
---
+++ src/components/Dialog.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="dialog">我是弹框</div>
+</template>
+<script>
+export default {}
+</script>
+
+<style scoped>
+.dialog {
+  width: 300px;
+  height: 300px;
+  padding: 30px;
+  background-color: #fff;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+}
+</style>
